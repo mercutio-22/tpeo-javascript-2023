8ff3bb1a931e68d2d06edf84f71940ff36562c8f
Aaryan Sagar <aaryansagar22@gmail.com>
2023-02-23 19:04:49 -0600

    added week 2 test.js code

---
+++ week_2/test.js
@@ -0,0 +1,19 @@
+//This function calculates area
+// function calculateTriangleArea(base, height) {
+//     return 1/2 * base * height
+// }
+
+// var area1 = calculateTriangleArea(3, 4)
+// console.log(area1)
+
+// var area2 = calculateTriangleArea(3.5, 4)
+// console.log(area2)
+
+function convertFeetToInches(length) {
+    return 12 * length
+}
+
+
+console.log(convertFeetToInches(1))
+console.log(convertFeetToInches(3))
+console.log(convertFeetToInches(30))
